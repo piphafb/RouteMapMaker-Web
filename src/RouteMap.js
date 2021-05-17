4fedaf6b6ccb26e9f1c9139a4e891d990d6ca81d
teamhimeh <thleaderh@gmail.com>
2021-05-17 22:45:35 +0900

    ADD: writing-modeをデータ構造に追加

--- src/RouteMap.js
+++ src/RouteMap.js
@@ -1,2 +1,7 @@
 
-export var routeMap = {stations: [{name: "A駅", pos: [10,50]}, {name: "B駅"}, {name: "C駅", pos: [90,50]}]}
+export var routeMap = 
+{stations: [
+    {name: "A駅", pos: [10,50], writingMode: "tb"}, 
+    {name: "B駅", writingMode: "tb"}, 
+    {name: "C駅", pos: [90,50], writingMode: "tb"}
+]}
