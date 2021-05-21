9277a366ca4a468fd332e0459bcc14810e5774d1
teamhimeh <thleaderh@gmail.com>
2021-05-21 22:58:32 +0900

    サンプルデータにtrainsを追加

--- src/RouteMap.js
+++ src/RouteMap.js
@@ -1,14 +1,30 @@
 
-export var routeMap = 
-{stations: [
+let stations = [
     {id: "CB0Ps3V0miG518CyogAfhg1u", name: "A駅", pos: [20,100], writingMode: "tb"}, 
     {id: "myvQgKLK52YIHNok0I2IGEeO", name: "B駅", writingMode: "tb"}, 
-    {id: "DTmW0q1DsR0X+iZ879AGD5Ro", name: "C駅", pos: [180,100], writingMode: "tb"}
-]};
+    {id: "DTmW0q1DsR0X+iZ879AGD5Ro", name: "C駅", pos: [180,100], writingMode: "tb"},
+];
+
+let trains = [{
+    id: "CXXfARIr5QyzHmadozGpo+it", 
+    name: "運転系統1", 
+    stops:[
+        {stationId: "CB0Ps3V0miG518CyogAfhg1u"},
+        {stationId: "myvQgKLK52YIHNok0I2IGEeO"},
+        {stationId: "DTmW0q1DsR0X+iZ879AGD5Ro"},
+    ],
+    strokeWidth: 2,
+    strokeColor: 0,
+    pointSize: 10,
+    pointStrokeColor: 0,
+    pointFillColor: 0,
+}];
+
+export let routeMap = {stations: stations, trains: trains};
 
 /*
 形式
-routeMap = {stations: [station]};
+routeMap = {stations: [station], trains: [train]};
 station = {
     id: String,
     name: string,
