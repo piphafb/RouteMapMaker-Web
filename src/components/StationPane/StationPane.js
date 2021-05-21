28457eb5589a5bb4d8399aabb68ca28e020d156d
teamhimeh <thleaderh@gmail.com>
2021-05-21 19:49:02 +0900

    StationPaneの関数コンポーネント化

--- src/components/StationPane/StationPane.js
+++ src/components/StationPane/StationPane.js
@@ -1,5 +1,5 @@
-import { RadioGroup, Radio, TextField, FormControlLabel } from '@material-ui/core';
-import React from 'react';
+import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
 import {
   CircularInput,
   CircularTrack,
@@ -13,50 +13,35 @@ import StationNameInput from './StationNameInput';
 import StationSelector from './StationSelector';
 import TextDirectionSelector from './TextDirectionSelector';
 
-class StationPane extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedIndex: 0 };
-  }
-
-  setNameOrientation = event => {
-    let idx = this.state.selectedIndex;
-    this.props.routeMap.stations[idx].writingMode = (event.target.value === "tate" ? "tb" : "lr");
-    this.props.updateView(this.props.routeMap);
-  }
-
-  render() {
-    var stations = this.props.routeMap.stations;
-    let selectedStation = stations[this.state.selectedIndex];
-    let params = {
-      routeMap: this.props.routeMap,
-      updateView: this.props.updateView,
-      selectedIndex: this.state.selectedIndex,
-      setSelectedIndex: ((i) => {this.setState({selectedIndex: i})})
-    }
-    return (
-      <div className="split">
-        <div className="split-left">
-          <AddStationButton {...params} />
-          <RemoveStationButton {...params} />
-          <StationSelector {...params}/>
-        </div>
-        <div className="split-right">
-          <StationNameInput {...params} />
-          <div className="prop-margin">
-            駅名の向き
-            <TextDirectionSelector {...params} />
-          </div>
-          <CircularInput className="circle-input">
-            <CircularTrack />
-            <CircularProgress />
-            <CircularThumb />
-          </CircularInput>
-          <TextField label="駅名の角度" defaultValue="0" />
-        </div>
+let StationPane = (props) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  let params = {
+    routeMap: props.routeMap,
+    updateView: props.updateView,
+    selectedIndex: selectedIndex,
+    setSelectedIndex: setSelectedIndex
+  };
+  return (
+    <div className="split">
+      <div className="split-left">
+        <AddStationButton {...params} />
+        <RemoveStationButton {...params} />
+        <StationSelector {...params}/>
       </div>
-    );
-  }
+      <div className="split-right">
+        <StationNameInput {...params} />
+        <div className="prop-margin">
+          駅名の向き
+          <TextDirectionSelector {...params} />
+        </div>
+        <CircularInput className="circle-input">
+          <CircularTrack />
+          <CircularProgress />
+          <CircularThumb />
+        </CircularInput>
+        <TextField label="駅名の角度" defaultValue="0" />
+      </div>
+    </div>
+  );
 }
-
 export default StationPane;
